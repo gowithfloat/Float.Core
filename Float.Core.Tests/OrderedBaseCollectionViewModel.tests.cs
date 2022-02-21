febc23a90bd929f7ec34ddf75fb29304f0d57742
steve <srichey@gowithfloat.com>
2022-02-21 11:17:33 -0600

    add OrderedBaseCollectionViewModel
    could be "BaseOrdered..." but either way is fine imo

---
+++ Float.Core.Tests/OrderedBaseCollectionViewModel.tests.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Float.Core.ViewModels;
+using Xunit;
+using System.Collections.ObjectModel;
+using Xunit.Abstractions;
+using System;
+
+namespace Float.Core.Tests
+{
+    public class OrderedBaseCollectionViewModelTests : XunitContextBase
+    {
+        class CollectionViewModel : OrderedBaseCollectionViewModel<TestModel, TestViewModel>
+        {
+            internal CollectionViewModel(IEnumerable<TestModel> modelCollection) : base(modelCollection)
+            {
+            }
+        }
+
+        public OrderedBaseCollectionViewModelTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void TestViewModelInsertOrder()
+        {
+            var coll = new ObservableCollection<TestModel>
+            {
+                new TestModel(0),
+                new TestModel(1)
+            };
+
+            var vm = new CollectionViewModel(coll);
+
+            coll.Insert(0, new TestModel(-1));
+
+            Assert.Equal(3, vm.Count());
+            Assert.Equal(-1, vm.ElementAt(0).Vmid);
+            Assert.Equal(0, vm.ElementAt(1).Vmid);
+            Assert.Equal(1, vm.ElementAt(2).Vmid);
+        }
+
+        [Fact]
+        public void TestViewModelMultipleInsertOrder()
+        {
+            var m0 = new TestModel(0);
+            var m1 = new TestModel(1);
+            var m2 = new TestModel(2);
+            var m3 = new TestModel(3);
+
+            var coll = new ObservableCollection<TestModel>
+            {
+                m2,
+            };
+
+            var vm = new CollectionViewModel(coll);
+
+            coll.Insert(m0.Mid, m0);
+            Assert.Equal(m0, vm.ElementAt(m0.Mid).UnderlyingModel);
+
+            coll.Insert(m1.Mid, m1);
+            Assert.Equal(m1, vm.ElementAt(m1.Mid).UnderlyingModel);
+
+            coll.Insert(m3.Mid, m3);
+            Assert.Equal(m3, vm.ElementAt(m3.Mid).UnderlyingModel);
+        }
+    }
+}
---
+++ Float.Core/ViewModels/OrderedBaseCollectionViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Float.Core.Collections;
+
+namespace Float.Core.ViewModels
+{
+    /// <summary>
+    /// Extends <see cref="BaseCollectionViewModel{TModel, TViewModel}"/> by inserting new view models at the same index as the underlying model.
+    /// </summary>
+    /// <typeparam name="TModel">The type of the model object associated with the viewmodel in this collection.</typeparam>
+    /// <typeparam name="TViewModel">The type of the viewmodel in this collection.</typeparam>
+    public class OrderedBaseCollectionViewModel<TModel, TViewModel> : BaseCollectionViewModel<TModel, TViewModel> where TModel : INotifyPropertyChanged where TViewModel : ViewModel<TModel>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderedBaseCollectionViewModel{TModel, TViewModel}"/> class.
+        /// </summary>
+        /// <param name="modelCollectionTask">The task which resolves to a model collection.</param>
+        protected OrderedBaseCollectionViewModel(Task<IEnumerable<TModel>> modelCollectionTask) : base(modelCollectionTask)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderedBaseCollectionViewModel{TModel, TViewModel}"/> class.
+        /// </summary>
+        /// <param name="modelCollection">Model collection.</param>
+        /// <param name="filter">An optional filter to apply to the model collection.</param>
+        protected OrderedBaseCollectionViewModel(IEnumerable<TModel> modelCollection, IFilter<TModel> filter = null) : base(modelCollection, filter)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void AddViewModels(IEnumerable modelCollection)
+        {
+            if (modelCollection == null)
+            {
+                return;
+            }
+
+            foreach (var model in modelCollection.OfType<TModel>())
+            {
+                if (ConvertModelToViewModel(model) is TViewModel viewModel)
+                {
+                    if (Models is IList list && list.IndexOf(model) is int idx && idx > -1)
+                    {
+                        ViewModels.Insert(idx, viewModel);
+                    }
+                    else
+                    {
+                        ViewModels.Add(viewModel);
+                    }
+
+                    if (HasSubscribers)
+                    {
+                        model.PropertyChanged += OnModelPropertyChanged;
+                    }
+                }
+            }
+        }
+    }
+}
