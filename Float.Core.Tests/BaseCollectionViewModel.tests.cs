95a16147c1f8f8fc7f971c10d32daaa75e5f3271
steve <srichey@gowithfloat.com>
2022-02-21 09:32:05 -0600

    add failing test for base collection view model order issue

---
+++ Float.Core.Tests/BaseCollectionViewModel.tests.cs
@@ -0,0 +1,97 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Float.Core.ViewModels;
+using Xunit;
+using System.Collections.ObjectModel;
+
+namespace Float.Core.Tests
+{
+    public class BaseCollectionViewModelTests
+    {
+        class SmallModel : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            internal SmallModel(int id) => this.Mid = id;
+
+            internal int Mid { get; }
+        }
+
+        class SmallViewModel : ViewModel<SmallModel>
+        {
+            public SmallViewModel(SmallModel model) : base(model)
+            {
+            }
+
+            internal int Vmid => Model.Mid;
+        }
+
+        class SmallCollectionViewModel : BaseCollectionViewModel<SmallModel, SmallViewModel>
+        {
+            internal SmallCollectionViewModel(IEnumerable<SmallModel> modelCollection) : base(modelCollection)
+            {
+            }
+        }
+
+        public BaseCollectionViewModelTests()
+        {
+        }
+
+        [Fact]
+        public void TestViewModelConstructionOrder()
+        {
+            var coll = new ObservableCollection<SmallModel>
+            {
+                new SmallModel(0),
+                new SmallModel(1)
+            };
+
+            var vm = new SmallCollectionViewModel(coll);
+
+            Assert.Equal(2, vm.Count());
+            Assert.Equal(0, vm.ElementAt(0).Vmid);
+            Assert.Equal(1, vm.ElementAt(1).Vmid);
+        }
+
+        [Fact]
+        public void TestViewModelAddOrder()
+        {
+            var coll = new ObservableCollection<SmallModel>
+            {
+                new SmallModel(0),
+                new SmallModel(1)
+            };
+
+            var vm = new SmallCollectionViewModel(coll);
+
+            coll.Add(new SmallModel(2));
+
+            Assert.Equal(3, vm.Count());
+            Assert.Equal(0, vm.ElementAt(0).Vmid);
+            Assert.Equal(1, vm.ElementAt(1).Vmid);
+            Assert.Equal(2, vm.ElementAt(2).Vmid);
+        }
+
+        [Fact]
+        public void TestViewModelInsertOrder()
+        {
+            var coll = new ObservableCollection<SmallModel>
+            {
+                new SmallModel(0),
+                new SmallModel(1)
+            };
+
+            var vm = new SmallCollectionViewModel(coll);
+
+            coll.Insert(0, new SmallModel(-1));
+
+            Assert.Equal(3, vm.Count());
+            Assert.Equal(-1, vm.ElementAt(0).Vmid);
+            Assert.Equal(0, vm.ElementAt(1).Vmid);
+            Assert.Equal(1, vm.ElementAt(2).Vmid);
+        }
+    }
+}
