19cde343738c63e30d9d86890b60aee51b619c72
Daniel Pfeiffer <dpfeiffer@gowithfloat.com>
2022-06-08 20:15:06 -0500

    Adds failing test for OAuthTokens

---
+++ Float.Core.Tests/OAuthTokens.tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Float.Core.Net;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Float.Core.Tests
+{
+    public class OAuthTokensTests
+    {
+        [Fact]
+        public async void Serializable()
+        {
+            var tokens = new OAuthTokens("access token", "refresh token", 10*60+1);
+            Assert.False(tokens.ShouldRefresh);
+            var serializedData = JsonConvert.SerializeObject(tokens);
+
+            // OAuthTokens are configured to proactively refresh 10 minutes before they expire.
+            // Since we've set our token time to 10 minutes and 1 second, after waiting just over
+            // one second here, the token should now think it's time to be refreshed.
+            await Task.Delay(1100);
+
+            var deserializedTokens = JsonConvert.DeserializeObject<OAuthTokens>(serializedData);
+
+            Assert.True(tokens.ShouldRefresh, "The test ran too fast");
+            Assert.True(deserializedTokens.ShouldRefresh, "The deserialized token has forgotten it should be refreshed");
+        }
+    }
+}
+
