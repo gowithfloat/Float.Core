dad31e44ef816b32e680c225d0070fbf133d05ba
Daniel Pfeiffer <dpfeiffer@gowithfloat.com>
2022-06-08 20:14:27 -0500

    Adds failing tests for RepeatableTaskContainer

---
+++ Float.Core.Tests/RepeatableTaskContainer.tests.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Float.Core.Tasks;
+using Xunit;
+
+namespace Float.Core.Tests
+{
+    public class RepeatableTaskContainerTests
+    {
+        [Fact]
+        public async Task TaskRunsOnce()
+        {
+            var counter = 0;
+            var repeatableTaskContainer = new RepeatableTaskContainer(async () =>
+            {
+                counter += 1;
+                await Task.Delay(10); // Simulate a "long" running task
+            });
+
+            await Task.WhenAll(
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run)
+                );
+
+            Assert.Equal(1, counter);
+        }
+
+        [Fact]
+        public async Task GenericTaskRunsOnce()
+        {
+            var counter = 0;
+            var repeatableTaskContainer = new RepeatableTaskContainer<string>(async () =>
+            {
+                counter += 1;
+                await Task.Delay(10);
+                return "test";
+            });
+
+            await Task.WhenAll(
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run),
+                Task.Run(repeatableTaskContainer.Run)
+                );
+
+            Assert.Equal(1, counter);
+        }
+    }
+}
+
